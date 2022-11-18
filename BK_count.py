2a9ca4183b9df427f99e947fe26e25640b2eafd9
Никита Василевский <n.vasilevsky@sibset-office.211.ru>
2022-11-18 13:50:06 +0700

    update counter

--- BK_count.py
+++ BK_count.py
@@ -1,4 +1,12 @@
 import collections
+import datetime
+from datetime import datetime
+
+def get_datatime() -> datetime:
+    date_format = '%H-%M %d-%m-%Y'
+    today = datetime.now()
+    today = today.strftime(date_format)
+    return today
 
 i = "start"
 barcode_list = []
@@ -7,11 +15,16 @@ while i != "stop":
     barcode = input("штрихкод: ")
     barcode_list.append(barcode)
     i = barcode
-    print(barcode_list)
+    #print(barcode_list)
 
 count = collections.Counter()
 for word in barcode_list:
     count[word] += 1
 
-print(count)
+now = get_datatime()
+
+with open(f"result{now}.txt", "w") as file:
+    file.write(str(count))
+
+#print(count)
 
