7f1fbcf86d22641837552ee47faddfc26b5cbf43
Nikita <nikirator@gmail.com>
2022-11-19 12:52:36 +0700

    add def to file

--- BK_checker.py
+++ BK_checker.py
@@ -3,36 +3,42 @@ import collections
 import datetime
 from datetime import datetime
 
+
 def get_datatime() -> datetime:
     date_format = '%H-%M %d-%m-%Y'
     today = datetime.now()
     today = today.strftime(date_format)
     return today
 
-i = "start"
 
-barcode_list = []
+def check_and_count():
+    i = "start"
+    barcode_list = []
 
-while i != "stop":
-    first = input("первый штрихкод: ")
-    playsound("sms.mp3", False)
-    i = first
+    while i != "stop":
+        first = input("первый штрихкод: ")
+        playsound("sms.mp3", False)
+        i = first
 
-    second = input("второй штрихкод: ")
+        second = input("второй штрихкод: ")
 
-    if first == second:
-        playsound("ok.mp3", False)
-        barcode_list.append(first)
-        print("ok")
-    else:
-        playsound("no.mp3", False)
-        print("no")
+        if first == second:
+            playsound("ok.mp3", False)
+            barcode_list.append(first)
+            print("ok")
+        else:
+            playsound("no.mp3", False)
+            print("no")
+    return barcode_list
 
-count = collections.Counter()
-for word in barcode_list:
-    count[word] += 1
 
-now = get_datatime()
+def count_barcode(barcode_list: list):
+    count = collections.Counter()
+    for word in barcode_list:
+        count[word] += 1
+    return count
 
-with open(f"result{now}.txt", "w") as file:
-    file.write(str(count))
+
+def save_to_file(count, time):
+    with open(f"result{time}.txt", "w") as file:
+        file.write(str(count))
